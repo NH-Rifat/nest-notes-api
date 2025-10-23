9704af3601d85a9fba7742039141cc4f8b9ecd3c
Hasan Rifat <rifatr729@gmail.com>
2025-10-23 17:40:13 +0600

    db connection intitated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(
+      process.env.MONGODB_URI || 'mongodb://localhost:27017/lms',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
